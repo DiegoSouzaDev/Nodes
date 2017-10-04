cdab5481845696d68b90894d432efc42deb88a93
Diego.Souza.Dev <diego.souza.dev@gmail.com>
2017-10-04 16:55:20 -0300

    parent=0 treated as a null. adding a exceptionHandler

---
+++ src/main/java/com/treasy/challenge/handler/RequestExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.treasy.challenge.handler;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class RequestExceptionHandler {
+
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public String emptyResultDataAccessHandler(EmptyResultDataAccessException e) {
+		return "Nó inexistente. Nenhum nó será removido";
+	}
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public String dataIntegrityHandler(EntityNotFoundException e) {
+		return "Não foi possivel encontrar o nó a ser atualizado ou o nó informado como 'parentId'";
+	}
+	
+}
--- src/main/java/com/treasy/challenge/service/NodeService.java
+++ src/main/java/com/treasy/challenge/service/NodeService.java
@@ -12,33 +12,33 @@ import com.treasy.challenge.repository.NodeRepository;
 
 @Service
 public class NodeService {
-	
+
 	@Autowired
 	private NodeRepository repos;
-	
+
 	public List<Node> findEntireTree() {
 		return repos.findEntireTree();
 	}
-	
+
 	public List<NodeDTO> findByParentId(final Long parentId) {
 		return convertToChildDTOList(repos.findByParentId(parentId));
 	}
-	
+
 	public Long saveNode(final NodeDTO nodeDTO) {
 		return repos.save(convertToNode(nodeDTO)).getId();
 	}
-	
+
 	public Long updateNode(final NodeDTO nodeDTO) {
 		return repos.save(convertToNode(nodeDTO)).getId();
 	}
-
+	
 	public void deleteNode(final Long id) {
 		repos.delete(id);
 	}
-	
+
 	private List<NodeDTO> convertToChildDTOList(final List<Node> returnedNodeList) {
 		final List<NodeDTO> nodeChildDTOList = new ArrayList<>();
-
+		
 		for (final Node node : returnedNodeList) {
 			final NodeDTO nodeChildDTO = new NodeDTO();
 			nodeChildDTO.setId(node.getId());
@@ -47,28 +47,29 @@ public class NodeService {
 			nodeChildDTO.setDetail(node.getDetail());
 			nodeChildDTO.setHasChildren(!node.getChildren().isEmpty());
 			nodeChildDTO.setParentId(node.getParent().getId());
-			
+
 			nodeChildDTOList.add(nodeChildDTO);
 		}
 		return nodeChildDTOList;
 	}
-	
+
 	public Node convertToNode(final NodeDTO nodeDTO) {
 		final Node node = new Node();
-		
+
 		node.setCode(nodeDTO.getCode());
 		node.setDescription(nodeDTO.getDescription());
 		node.setDetail(nodeDTO.getDetail());
-		
+
 		if (nodeDTO.getId() != null) {
 			node.setId(nodeDTO.getId());
 		}
-		
-		if (nodeDTO.getParentId() != null) {
+
+		if (nodeDTO.getParentId() != null && nodeDTO.getParentId() != 0) {
 			final Node parent = new Node();
 			parent.setId(nodeDTO.getParentId());
 			node.setParent(parent);
 		}
+		
 		return node;
 	}
 }
